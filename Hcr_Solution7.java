ebc7ac223254d142feefc4cc1c1f97b72f5495db
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-08 16:43:09 +0530

    Add files via upload

---
+++ Hcr_Solution7.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+class Hcr_Solution7
+{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            // write code here
+            for(int j=0; j<n; j++)
+            {
+                a = a + (int)Math.pow(2,j)*b;
+                System.out.print(a+" ");
+            }
+            System.out.println();            
+            
+        }
+        in.close();
+    }
+}
